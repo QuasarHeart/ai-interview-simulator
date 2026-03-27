22a5cc8c915c7f59fa0f45b4025bf48dae4724c4
feixingxr <2979734778@qq.com>
2026-03-27 20:09:25 +0800

    test

--- src/main/java/org/buhuiqiming/fuchuang/dto/GenerateReportResponse.java
+++ src/main/java/org/buhuiqiming/fuchuang/dto/GenerateReportResponse.java
@@ -1,10 +1,9 @@
 package org.buhuiqiming.fuchuang.dto;
 
-import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+import lombok.NoArgsConstructor;
 import tools.jackson.databind.PropertyNamingStrategies;
 import tools.jackson.databind.annotation.JsonNaming;
 
@@ -12,6 +11,8 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class GenerateReportResponse {
     private String hiringRecommendation;
