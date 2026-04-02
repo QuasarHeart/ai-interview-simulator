d922ed1efb4f20935a09c43cdaf8e50edcd6aff9
feixingxr <2979734778@qq.com>
2026-04-02 19:09:13 +0800

    test

--- src/main/java/org/buhuiqiming/fuchuang/controller/LiveKitController.java
+++ src/main/java/org/buhuiqiming/fuchuang/controller/LiveKitController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
+@RestController("/api/livekit")
 public class LiveKitController {
     private  final LiveKitService liveKitService;
     private final WebhookReceiver webhookReceiver;
