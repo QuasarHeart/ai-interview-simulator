72bf541b276353ca554faca33401ddb190bad34d
feixingxr <2979734778@qq.com>
2026-04-09 10:20:36 +0800

    modify

--- src/main/java/org/buhuiqiming/fuchuang/service/ServiceImpl/LiveKitServiceImpl.java
+++ src/main/java/org/buhuiqiming/fuchuang/service/ServiceImpl/LiveKitServiceImpl.java
@@ -88,7 +88,7 @@ public class LiveKitServiceImpl implements LiveKitService {
                 new RoomName(roomName)
         );
         return Map.of(
-                "token", token.toJwt(),
+                "livekitToken", token.toJwt(),
                 "room", roomName,
                 "url", host.replace("https://", "wss://")
         );
